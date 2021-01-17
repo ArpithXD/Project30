4299fbfee49d03fc440b2650345c7e9c106d7e30
Arpith Sampath Kumar <74534441+ArpithXD@users.noreply.github.com>
2021-01-18 12:48:18 +1300

    Add files via upload

---
+++ block1.js
@@ -0,0 +1,28 @@
+class Block1 {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      this.Visibility = 255;
+    }
+    display(){
+      if (this.body.speed < 3) {
+        this.body.display();
+      } else {
+        World.remove(world, this.body);
+        push();
+        this.Visibility = this.Visibility - 5;
+        tint(255, this.Visibility);
+        image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+        pop();
+      }
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("yellow");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
